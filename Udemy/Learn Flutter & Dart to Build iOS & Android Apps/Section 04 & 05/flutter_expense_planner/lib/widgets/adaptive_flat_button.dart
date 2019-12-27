9e62fcf54b55cb68bb4c55c3c1ac975c9ce2fa8f
Jason <bigpel66@gmail.com>
2019-12-28 01:09:20 +0900

    adaptive button

---
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 04 & 05/flutter_expense_planner/lib/widgets/adaptive_flat_button.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdaptiveFlatButton extends StatelessWidget {
+  final String text;
+  final Function datePickerHandler;
+
+  AdaptiveFlatButton({@required this.text, @required this.datePickerHandler});
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            child: Text(text,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                )),
+            onPressed: () => datePickerHandler(),
+          )
+        : FlatButton(
+            textColor: Theme.of(context).primaryColor,
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onPressed: () => datePickerHandler());
+  }
+}
--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 04 & 05/flutter_expense_planner/lib/widgets/new_transaction.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 04 & 05/flutter_expense_planner/lib/widgets/new_transaction.dart
@@ -1,7 +1,8 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_expense_planner/widgets/adaptive_flat_button.dart';
 import 'package:intl/intl.dart';
+import '../widgets/adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTransactionHandler;
@@ -92,22 +93,7 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
-                    Platform.isIOS
-                        ? CupertinoButton(
-                            color: Colors.amber,
-                            child: Text('Choose Date',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                )),
-                            onPressed: () => _presentDatePicker(),
-                          )
-                        : FlatButton(
-                            textColor: Theme.of(context).primaryColor,
-                            child: Text(
-                              'Choose Date',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            onPressed: () => _presentDatePicker()),
+                    AdaptiveFlatButton(text: 'Choose Date', datePickerHandler: _presentDatePicker,)
                   ],
                 ),
               ),
