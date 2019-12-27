e593db9ddf589125ee8aea6163da20487254376b
Jason <bigpel66@gmail.com>
2019-12-27 16:25:57 +0900

    platform button testing

--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 04 & 05/flutter_expense_planner/lib/main.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 04 & 05/flutter_expense_planner/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 import './widgets/transaction_list.dart';
@@ -187,7 +188,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text('Show Chart'),
-                  Switch(
+                  Switch.adaptive(
+                    activeColor: Theme.of(context).accentColor,
                     value: _showChart,
                     onChanged: (value) => setState(() {
                       _showChart = value;
@@ -218,10 +220,12 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => _startAddNewTransaction(context),
-      ),
+      floatingActionButton: Platform.isIOS
+          ? Container()
+          : FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () => _startAddNewTransaction(context),
+            ),
     );
   }
 }
