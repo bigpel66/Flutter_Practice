2397d0a3b77cd0864c444300c3b3aa69372eadbf
Jason <bigpel66@gmail.com>
2019-12-30 15:02:36 +0900

    initial setting for the meals app

---
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 07/flutter_meals_app/lib/main.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget _buildMaterialApp() {
+    return MaterialApp(
+      title: 'Practice with Udemy',
+      home: MyHomePage(),
+    );
+  }
+
+  Widget _buildCupertinoApp() {
+    return CupertinoApp(
+      title: 'Practice with Udemy',
+      home: MyHomePage(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // return Platform.isIOS ? _buildCupertinoApp() : _buildMaterialApp();
+    return _buildMaterialApp();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget _buildAppBar() {
+    return AppBar(
+      title: Text('MEALS App'),
+    );
+  }
+
+  Widget _buildCupertinoNavigationBar() {
+    return CupertinoNavigationBar(
+      middle: Text('MEALS APP'),
+    );
+  }
+
+  Widget _buildScaffold(PreferredSizeWidget appBar) {
+    return Scaffold(
+      appBar: appBar,
+      body: Container(),
+    );
+  }
+
+  Widget _buildCupertinoPageScaffold(PreferredSizeWidget appBar) {
+    return CupertinoPageScaffold(
+      navigationBar: appBar,
+      child: Container(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final PreferredSizeWidget appBar =
+        Platform.isIOS ? _buildCupertinoNavigationBar() : _buildAppBar();
+
+    final mediaQuery = MediaQuery.of(context);
+
+    return Platform.isIOS
+        ? _buildCupertinoPageScaffold(appBar)
+        : _buildScaffold(appBar);
+  }
+}
