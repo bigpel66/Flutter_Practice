574db899a9fd83875e434f5ff8768f18560deba5
Jason <bigpel66@gmail.com>
2019-12-30 17:36:38 +0900

    navigator for the category item

--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 07/flutter_meals_app/lib/categories_screen.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 07/flutter_meals_app/lib/categories_screen.dart
@@ -5,16 +5,19 @@ import './dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(25),
-      children: DUMMY_CATEGORIES.map((data) {
-        return CategoryItem(title: data.title, color: data.color);
-      }).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeals')),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES.map((data) {
+          return CategoryItem(title: data.title, color: data.color);
+        }).toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 07/flutter_meals_app/lib/main.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 07/flutter_meals_app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+// import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_meals_app/categories_screen.dart';
@@ -8,49 +8,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  Widget _buildAppBar() {
-    return AppBar(
-      title: const Text('DeliMeals'),
-    );
-  }
+  // Widget _buildCupertinoApp() {
+  //   return CupertinoApp(
+  //     title: 'Practice with Udemy',
+  //     home: CategoriesScreen(),
+  //   );
+  // }
 
-  Widget _buildCupertinoNavigationBar() {
-    return CupertinoNavigationBar(
-      middle: const Text('DeliMeals'),
-    );
-  }
-
-  Widget _buildScaffold(PreferredSizeWidget appBar) {
-    return Scaffold(
-      appBar: appBar,
-      body: Center(
-        child: CategoriesScreen(),
-      ),
-    );
-  }
-
-  Widget _buildCupertinoPageScaffold(PreferredSizeWidget appBar) {
-    return CupertinoPageScaffold(
-      navigationBar: appBar,
-      child: Center(
-        child: Text('Navigation Time'),
-      ),
-    );
-  }
-
-  Widget _buildMaterialApp(PreferredSizeWidget appBar) {
+  Widget _buildMaterialApp() {
     return MaterialApp(
       title: 'Practice with Udemy',
-      home: _buildScaffold(appBar),
+      home: CategoriesScreen(),
       theme: ThemeData(
         primarySwatch: Colors.pink,
         accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-              body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              title: TextStyle(
+              body1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              body2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              title: const TextStyle(
                 fontSize: 18,
                 fontFamily: 'RobotoCondensed',
                 fontWeight: FontWeight.bold,
@@ -64,22 +41,10 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  Widget _buildCupertinoApp(PreferredSizeWidget appBar) {
-    return CupertinoApp(
-      title: 'Practice with Udemy',
-      home: _buildCupertinoPageScaffold(appBar),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    // final PreferredSizeWidget appBar =
-    //     Platform.isIOS ? _buildCupertinoNavigationBar() : _buildAppBar();
-
-    final PreferredSizeWidget appBar = _buildAppBar();
-
     // return Platform.isIOS ? _buildCupertinoApp(appBar) : _buildMaterialApp(appBar);
 
-    return _buildMaterialApp(appBar);
+    return _buildMaterialApp();
   }
 }
