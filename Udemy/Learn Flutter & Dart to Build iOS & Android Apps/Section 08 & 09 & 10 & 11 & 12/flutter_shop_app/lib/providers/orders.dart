25effe3df74cfe228b2c374496c1c2388b80131e
Jason <bigpel66@gmail.com>
2020-01-04 15:59:59 +0900

    order item and list added

---
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 08 & 09 & 10 & 11 & 12/flutter_shop_app/lib/providers/orders.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './cart.dart';
+
+class OrderItem {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  OrderItem({
+    @required this.id,
+    @required this.amount,
+    @required this.products,
+    @required this.dateTime,
+  });
+}
+
+class Orders with ChangeNotifier {
+  List<OrderItem> _orders = [];
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+
+  void _addOrder(List<CartItem> products, double amount) {
+    _orders.insert(
+      0,
+      OrderItem(
+        id: DateTime.now().toString(),
+        amount: amount,
+        products: products,
+        dateTime: DateTime.now(),
+      ),
+    );
+    notifyListeners();
+  }
+}
