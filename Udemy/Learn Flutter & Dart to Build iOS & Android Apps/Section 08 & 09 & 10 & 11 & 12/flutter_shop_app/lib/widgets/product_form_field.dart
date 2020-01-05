1df381070647557f7d4b2fe3eb0b00e372618a54
Jason <bigpel66@gmail.com>
2020-01-05 17:39:49 +0900

    image preview updates when it loses the focus: implemented by adding FocusNode and listener on the FocusNode

--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 08 & 09 & 10 & 11 & 12/flutter_shop_app/lib/widgets/product_form_field.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 08 & 09 & 10 & 11 & 12/flutter_shop_app/lib/widgets/product_form_field.dart
@@ -9,12 +9,29 @@ class ProductFormField extends StatefulWidget {
 class _ProductFormFieldState extends State<ProductFormField> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(() => _updateImageUrl());
+    super.initState();
+  }
 
   @override
   void dispose() {
+    _imageUrlFocusNode.removeListener(() => _updateImageUrl());
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.dispose();
+    _imageUrlController.dispose();
     super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
   }
 
   @override
@@ -48,6 +65,40 @@ class _ProductFormFieldState extends State<ProductFormField> {
               keyboardType: TextInputType.multiline,
               maxLines: 3,
               focusNode: _descriptionFocusNode,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(right: 10, top: 8),
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  child: _imageUrlController.text.isEmpty
+                      ? Center(child: Text('Enter a URL'))
+                      : FittedBox(
+                          child: Image.network(
+                            _imageUrlController.text,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Image URL'),
+                    keyboardType: TextInputType.url,
+                    textInputAction: TextInputAction.done,
+                    controller: _imageUrlController,
+                    focusNode: _imageUrlFocusNode,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
