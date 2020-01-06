3b12063e7ce3abe00d353c3b79e2b3935d190f56
Jason <bigpel66@gmail.com>
2020-01-06 23:24:10 +0900

    fetching data from firebase and checking the data

--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 08 & 09 & 10 & 11 & 12/flutter_shop_app/lib/providers/products.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 08 & 09 & 10 & 11 & 12/flutter_shop_app/lib/providers/products.dart
@@ -55,6 +55,17 @@ class Products with ChangeNotifier {
     });
   }
 
+  Future<void> fetchAndSetProducts() async {
+    const url = 'https://flutter-shop-app-114ec.firebaseio.com/products.json';
+
+    try {
+      final response = await http.get(url);
+      print(json.decode(response.body));
+    } catch (error) {
+      throw error;
+    }
+  }
+
   Future<void> addProduct(Product product) async {
     const url = 'https://flutter-shop-app-114ec.firebaseio.com/products.json';
 
--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 08 & 09 & 10 & 11 & 12/flutter_shop_app/lib/widgets/products_grid.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 08 & 09 & 10 & 11 & 12/flutter_shop_app/lib/widgets/products_grid.dart
@@ -3,16 +3,41 @@ import 'package:provider/provider.dart';
 import './product_item.dart';
 import '../providers/products.dart';
 
-class ProductsGrid extends StatelessWidget {
+class ProductsGrid extends StatefulWidget {
   final bool showOnlyFavorites;
 
   ProductsGrid({@required this.showOnlyFavorites});
 
   @override
+  _ProductsGridState createState() => _ProductsGridState();
+}
+
+class _ProductsGridState extends State<ProductsGrid> {
+  bool _isInit = true;
+  // @override
+  // void initState() {
+  // Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  // Future.delayed(Duration.zero).then((_) {
+  //   Provider.of<Products>(context).fetchAndSetProducts();
+  // });
+  //   super.initState();
+  // }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      Provider.of<Products>(context).fetchAndSetProducts();
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products =
-        showOnlyFavorites ? productsData.favoritedItems : productsData.items;
+    final products = widget.showOnlyFavorites
+        ? productsData.favoritedItems
+        : productsData.items;
 
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
