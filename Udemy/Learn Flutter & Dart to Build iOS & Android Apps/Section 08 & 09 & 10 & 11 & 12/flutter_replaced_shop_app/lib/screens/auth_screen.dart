e1c1088ab7289fb0a2dbd921a3c0630f8c8498f7
Jason <bigpel66@gmail.com>
2020-01-08 22:40:16 +0900

    manual animation changed into animatedbuilder

--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 08 & 09 & 10 & 11 & 12/flutter_replaced_shop_app/lib/screens/auth_screen.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 08 & 09 & 10 & 11 & 12/flutter_replaced_shop_app/lib/screens/auth_screen.dart
@@ -111,19 +111,19 @@ class _AuthCardState extends State<AuthCard>
     _animationController = AnimationController(
       vsync: this,
       duration: Duration(
-        milliseconds: 300,
+        milliseconds: 450,
       ),
     );
     _heightAnimation = Tween<Size>(
       begin: Size(double.infinity, 280),
-      end: Size(double.infinity, 300),
+      end: Size(double.infinity, 320),
     ).animate(
       CurvedAnimation(
         parent: _animationController,
         curve: Curves.fastOutSlowIn,
       ),
     );
-    _heightAnimation.addListener(() => setState(() {}));
+    // _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -220,12 +220,19 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _heightAnimation.value.height,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (context, child) {
+          return Container(
+            height: _heightAnimation.value.height,
+            constraints: BoxConstraints(
+              minHeight: _heightAnimation.value.height,
+            ),
+            width: deviceSize.width * 0.75,
+            padding: EdgeInsets.all(16.0),
+            child: child,
+          );
+        },
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
