56087dd43b636124519f1a605de3dcf1249f488e
Jason <bigpel66@gmail.com>
2020-01-09 16:25:51 +0900

    local database SQLite && google map input form

--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 13 & 14/flutter_using_native_app/lib/screens/add_place_screen.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 13 & 14/flutter_using_native_app/lib/screens/add_place_screen.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../widgets/image_input.dart';
 import '../providers/great_places.dart';
+import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -49,6 +50,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     ),
                     const SizedBox(height: 20),
                     ImageInput(selectHandler: _selectImage),
+                    const SizedBox(height: 10),
+                    LocationInput(),
                   ],
                 ),
               ),
