ced619211ce300ac9dbdf4fa046706a104de3c4e
Jason <bigpel66@gmail.com>
2020-01-09 16:52:23 +0900

    gm api & location helper & current location

---
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 13 & 14/flutter_using_native_app/lib/helpers/location_helper.dart
@@ -0,0 +1,12 @@
+import 'package:sqflite/utils/utils.dart';
+
+const GOOGLE_API_KEY = 'AIzaSyCAN4eRMxmnsJRTHdXCE7NTCs_ji0WrI5Y';
+
+class LocationHelper {
+  static String generateLocationPreviewImage({
+    double latitude,
+    double longitude,
+  }) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+}
