776242f9121c337e6c6182e03bfb63ef6960a0e3
Jason <bigpel66@gmail.com>
2020-01-10 14:00:34 +0900

    icon / splash / application name: real android, virtual android, virtual ios

--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 13/flutter_using_native_app/ios/Runner/AppDelegate.swift
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 13/flutter_using_native_app/ios/Runner/AppDelegate.swift
@@ -9,6 +9,9 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyCAN4eRMxmnsJRTHdXCE7NTCs_ji0WrI5Y")
+    var flutter_native_splash = 1
+    UIApplication.shared.isStatusBarHidden = false
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
