0c0c48614de675037262b820633bf79ad01a07fb
Jason <bigpel66@gmail.com>
2020-01-26 22:25:00 +0900

    new proj

---
+++ Udemy/The Complete 2020 Flutter Development Bootcamp with Dart/BMI_Calculator/flutter_bmi_calc/lib/input_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: Theme(
+        data: ThemeData(accentColor: Colors.purple),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
