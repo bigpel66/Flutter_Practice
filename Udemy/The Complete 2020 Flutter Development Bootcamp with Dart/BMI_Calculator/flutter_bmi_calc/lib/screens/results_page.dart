98482626e8a92edb052576c5d8c2bce302573239
Jason <bigpel66@gmail.com>
2020-01-28 13:34:11 +0900

    letter uppercase

--- Udemy/The Complete 2020 Flutter Development Bootcamp with Dart/BMI_Calculator/flutter_bmi_calc/lib/screens/results_page.dart
+++ Udemy/The Complete 2020 Flutter Development Bootcamp with Dart/BMI_Calculator/flutter_bmi_calc/lib/screens/results_page.dart
@@ -38,7 +38,7 @@ class ResultsPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Text('$bmiResult', style: resultMeasureTextStyle),
+                  Text('${bmiResult.toUpperCase()}', style: resultMeasureTextStyle),
                   Text('$bmiMeasure', style: resultBMITextStyle),
                   Text(
                     '$bmiDescription',
