148ed9c98873b11c5106fc5b0ab31c4598d9a0aa
Jason <bigpel66@gmail.com>
2020-01-29 10:18:57 +0900

    bronze level

--- Udemy/The Complete 2020 Flutter Development Bootcamp with Dart/Bitcoin_Ticker/flutter_bitcoin_ticker/lib/price_screen.dart
+++ Udemy/The Complete 2020 Flutter Development Bootcamp with Dart/Bitcoin_Ticker/flutter_bitcoin_ticker/lib/price_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import './coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +9,61 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  String bitcoinValueInUSD = '?';
+
+  Widget getPicker() {
+    if (Platform.isIOS)
+      return CupertinoPicker(
+        backgroundColor: Colors.lightBlue,
+        itemExtent: 32.0,
+        onSelectedItemChanged: (index) {},
+        children: [
+          ...currenciesList.map((item) {
+            return Text(
+              item,
+            );
+          }),
+        ],
+      );
+    else
+      return DropdownButton<String>(
+        value: selectedCurrency,
+        items: [
+          ...currenciesList.map((item) {
+            return DropdownMenuItem(
+              value: item,
+              child: Text(item),
+            );
+          })
+        ],
+        onChanged: (value) {
+          setState(() {
+            selectedCurrency = value;
+          });
+        },
+      );
+  }
+
+  void getData() async {
+    try {
+      double data = await CoinData().getCoinData();
+
+      setState(() {
+        bitcoinValueInUSD = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +85,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitcoinValueInUSD USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,7 +100,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: getPicker(),
           ),
         ],
       ),
