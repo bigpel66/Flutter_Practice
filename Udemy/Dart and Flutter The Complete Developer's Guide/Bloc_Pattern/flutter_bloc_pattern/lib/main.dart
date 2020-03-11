a5161d967d14e7ed3f783e024f73961cbd7385da
BIGPEL <bigpel66@gmail.com>
2020-03-11 11:30:11 +0900

    understanding the concept of BLOC and streams && understanding how the provider made and inherited widget

--- Udemy/Dart and Flutter The Complete Developer's Guide/Bloc_Pattern/flutter_bloc_pattern/lib/main.dart
+++ Udemy/Dart and Flutter The Complete Developer's Guide/Bloc_Pattern/flutter_bloc_pattern/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import './screens/login_screen.dart';
+import './blocs/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Log Me In',
-      home: Scaffold(
-        body: LoginScreen(),
+    return Provider(
+      child: MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
       ),
     );
   }
