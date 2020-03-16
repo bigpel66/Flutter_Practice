9387d6ea4b40c8b018f1dd9d69f2a2073f8cee5a
BIGPEL <bigpel66@gmail.com>
2020-03-16 17:30:02 +0900

    future data with listview builder to make better rendering things: need to make bloc things

--- Udemy/Dart and Flutter The Complete Developers Guide/Database/flutter_database/lib/src/screens/news_list.dart
+++ Udemy/Dart and Flutter The Complete Developers Guide/Database/flutter_database/lib/src/screens/news_list.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 
 class NewsList extends StatelessWidget {
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (ctx, index) {
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (ctx, snapshot) {
+            return Container(
+              height: 80,
+              child: snapshot.hasData
+                  ? Text('I\'m visible')
+                  : Text('I haven\'t fetched data yet'),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future<void> getFuture() {
+    return Future.delayed(Duration(seconds: 2), () {
+      return 'hi';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show some news here'),
+      body: buildList(),
     );
   }
 }
--- Udemy/Dart and Flutter The Complete Developers Guide/Database/flutter_database/test/widget_test.dart
+++ Udemy/Dart and Flutter The Complete Developers Guide/Database/flutter_database/test/widget_test.dart
@@ -1,30 +1,36 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'dart:convert';
+ import 'package:flutter_database/src/resources/news_api_provider.dart';
+ import 'package:flutter_test/flutter_test.dart';
+ import 'package:http/http.dart';
+ import 'package:http/testing.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+ void main() {
+   test('FetchTopIds returns a list of ids', () async {
+     final newsApi = NewsApiProvider();
+     newsApi.client = MockClient((request) async {
+       return Response(
+         json.encode([1, 2, 3, 4]),
+         200,
+       );
+     });
 
-import 'package:flutter_database/main.dart';
+     final ids = await newsApi.fetchTopIds();
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+     expect(ids, [1, 2, 3, 4]);
+   });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+   test('FetchItem returns a item model', () async {
+     final newsApi = NewsApiProvider();
+     newsApi.client = MockClient((request) async {
+       final jsonMap = {'id': 123};
+       return Response(
+         json.encode(jsonMap),
+         200,
+       );
+     });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+     final item = await newsApi.fetchItem(999);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+     expect(item.id, 123);
+   });
+ }
