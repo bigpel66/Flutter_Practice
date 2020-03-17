39f1dc410f53bfef3b191def41a12c418c53d0be
BIGPEL <bigpel66@gmail.com>
2020-03-17 14:28:03 +0900

    news list tile

--- Udemy/Dart and Flutter The Complete Developers Guide/Database/flutter_database/lib/src/blocs/stories_bloc.dart
+++ Udemy/Dart and Flutter The Complete Developers Guide/Database/flutter_database/lib/src/blocs/stories_bloc.dart
@@ -3,8 +3,13 @@ import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class StoriesBloc {
+  StoriesBloc() {
+    _items.stream.transform(_itemsTransformer());
+  }
+
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
 
   Stream<List<int>> get topIds => _topIds.stream;
 
@@ -13,14 +18,20 @@ class StoriesBloc {
     _topIds.sink.add(ids);
   }
 
-  _itemTransformer() {
+  Function(int) get fetchItem => _items.sink.add;
+
+  _itemsTransformer() {
     return ScanStreamTransformer(
-      (Map<int, Future<ItemModel>> cache, int id, _) {},
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
       <int, Future<ItemModel>>{},
     );
   }
 
   void dispose() {
     _topIds.close();
+    _items.close();
   }
 }
