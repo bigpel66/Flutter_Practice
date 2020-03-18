1b62977dd327c3ed7b584a3598504f5f227c782c
BIGPEL <bigpel66@gmail.com>
2020-03-18 17:07:56 +0900

    on generate navigating

--- Udemy/Dart and Flutter The Complete Developers Guide/Database/flutter_database/lib/src/screens/news_detail.dart
+++ Udemy/Dart and Flutter The Complete Developers Guide/Database/flutter_database/lib/src/screens/news_detail.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_database/src/widgets/loading_container.dart';
+import '../blocs/comments_provider.dart';
+import '../models/item_model.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
 
   NewsDetail({this.itemId});
 
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.commentsOutput,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return LoadingContainer();
+        }
+
+        final itemFuture = snapshot.data[itemId];
+
+        print(itemFuture);
+
+        return FutureBuilder(
+          future: itemFuture,
+          builder: (context, itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return LoadingContainer();
+            }
+
+            return Text(itemSnapshot.data.title);
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail'),
       ),
-      body: Text('$itemId'),
+      body: buildBody(bloc),
     );
   }
 }
