e500fc739472e829911bd1ed0e5a08bdfbe46d81
BIGPEL <bigpel66@gmail.com>
2020-03-19 11:55:10 +0900

    stories completed

--- Udemy/Dart and Flutter The Complete Developers Guide/Database/flutter_database/lib/src/widgets/comment.dart
+++ Udemy/Dart and Flutter The Complete Developers Guide/Database/flutter_database/lib/src/widgets/comment.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import '../models/item_model.dart';
+import 'loading_container.dart';
 
 class Comment extends StatelessWidget {
   final int itemId;
@@ -24,7 +25,7 @@ class Comment extends StatelessWidget {
       future: itemMap[itemId],
       builder: (ctx, snapshot) {
         if (!snapshot.hasData) {
-          return Text('Still Loading Comment');
+          return LoadingContainer();
         }
 
         final item = snapshot.data;
