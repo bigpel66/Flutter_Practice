8867f583578c4567580dc08b8920253ea113e591
BIGPEL <bigpel66@gmail.com>
2020-03-20 09:01:43 +0900

    multiple choice for converting selection

--- Udemy/Dart and Flutter The Complete Developers Guide/Pure_Dart_Converter/converter/bin/main.dart
+++ Udemy/Dart and Flutter The Complete Developers Guide/Pure_Dart_Converter/converter/bin/main.dart
@@ -1,14 +1,20 @@
 import 'dart:io';
 import 'package:prompter_sg/prompter_sg.dart';
 
+List<Option> buildFormatOptions() {
+  return [
+    Option('Convert to jpeg', 'jpeg'),
+    Option('Convert to png', 'png'),
+  ];
+}
+
 void main() {
   final prompter = Prompter();
-
-  List<Option> options = [];
 
   bool answer = prompter.askBinary('Are you here to convert an image?');
 
   if (!answer) {
     exit(0);
   }
+  prompter.askMultiple('Select format: ', buildFormatOptions());
 }
