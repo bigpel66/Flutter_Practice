8955c0984af7651ffaa9079913afe5cf73938b2e
BIGPEL <bigpel66@gmail.com>
2020-05-26 15:24:29 +0900

    login and sign up form

--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 14/flutter_chatting_application/lib/widgets/auth-form.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 14/flutter_chatting_application/lib/widgets/auth-form.dart
@@ -7,6 +7,20 @@ class AuthForm extends StatefulWidget {
 
 class _AuthFormState extends State<AuthForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState.save();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,19 +36,50 @@ class _AuthFormState extends State<AuthForm> {
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (value) {
+                      _userEmail = value;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       labelText: 'Email Address',
                       hintText: 'Enter Email Address (abc@abc.com)',
                     ),
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'User Name',
-                      hintText: 'Enter User Name',
+                  if (!_isLogin)
+                    TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 characters.';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (value) {
+                        _userName = value;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'User Name',
+                        hintText: 'Enter User Name',
+                      ),
                     ),
-                  ),
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long.';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (value) {
+                      _userPassword = value;
+                    },
                     decoration: InputDecoration(
                       labelText: 'Password',
                       hintText: 'Enter Password',
@@ -43,12 +88,20 @@ class _AuthFormState extends State<AuthForm> {
                   ),
                   const SizedBox(height: 12.0),
                   RaisedButton(
-                    onPressed: () {},
-                    child: Text('Login'),
+                    onPressed: () {
+                      _trySubmit();
+                    },
+                    child: Text(_isLogin ? 'Login' : 'Signup'),
                   ),
                   FlatButton(
-                    onPressed: () {},
-                    child: Text('Create a New Accont'),
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                    child: Text(_isLogin
+                        ? 'Create a New Accont'
+                        : 'I already Have an Account'),
                     textColor: Theme.of(context).primaryColor,
                   ),
                 ],
