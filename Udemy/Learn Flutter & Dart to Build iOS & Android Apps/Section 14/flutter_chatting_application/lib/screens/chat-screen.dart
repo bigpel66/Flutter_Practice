2ef2f09ac91c992e8a3a9c4d907a3f51701589f8
BIGPEL <bigpel66@gmail.com>
2020-05-28 03:59:48 +0900

    notification fore, back, terminated / subscribe notification event / fcf notification event maker

--- Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 14/flutter_chatting_application/lib/screens/chat-screen.dart
+++ Udemy/Learn Flutter & Dart to Build iOS & Android Apps/Section 14/flutter_chatting_application/lib/screens/chat-screen.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import '../widgets/chat/messages.dart';
 import '../widgets/chat/new-message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure(
+      onMessage: (msg) {
+        print(msg);
+        print(1);
+        return;
+      },
+      onLaunch: (msg) {
+        print(msg);
+        print(2);
+        return;
+      },
+      onResume: (msg) {
+        print(msg);
+        print(3);
+        return;
+      },
+    );
+    fbm.subscribeToTopic('chat');
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
