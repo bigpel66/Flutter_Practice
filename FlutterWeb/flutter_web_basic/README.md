0d2726195b1309b01cf1d36119370dfcd20f4b31
BIGPEL <bigpel66@gmail.com>
2020-09-19 00:23:41 +0900

    flutter web basic part 1

---
+++ FlutterWeb/flutter_web_basic/README.md
@@ -0,0 +1,16 @@
+# flutter_web_basic
+
+Flutter Web Practice to Build Simple Introduction Page
+
+## How to Setup
+
+1. flutter channel master
+2. flutter upgrade
+3. flutter doctor
+4. flutter config --enable-web
+5. flutter devices
+6. flutter create flutter_web_basic
+
+## How to Build Web
+
+flutter build web
---
+++ FlutterWeb/flutter_web_basic/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_basic/views/home/home_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Basic',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: Theme.of(context).textTheme.apply(fontFamily: 'Open Sans'),
+      ),
+      home: HomeView(),
+    );
+  }
+}
