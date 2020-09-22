7d70c0e40f941b2ed5614c2c65bd3cf8504cb3ed
BIGPEL <bigpel66@gmail.com>
2020-09-22 17:46:30 +0900

    modified orders

--- FilledStack/custom_dialog/lib/main.dart
+++ FilledStack/custom_dialog/lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:custom_dialog/ui/setup_dialog_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:custom_dialog/views/home/home_view.dart';
 import 'package:custom_dialog/services/locator.dart';
+import 'package:custom_dialog/ui/setup_dialog_ui.dart';
 
 void main() {
   setupLocator();
--- FilledStack/custom_dialog/lib/services/locator.dart
+++ FilledStack/custom_dialog/lib/services/locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+
 import 'locator.config.dart';
 
 final GetIt locator = GetIt.instance;
